472ab2f8dfe34b8b2574acaca38168f0f611f504
HimGH <him11jul21@gmail.com>
2024-08-08 21:48:29 +0530

    Unit Test Update

--- rsystem-ui/src/app/component/story/story.component.spec.ts
+++ rsystem-ui/src/app/component/story/story.component.spec.ts
@@ -19,7 +19,5 @@ describe('StoryComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  
 });
--- rsystem-ui/src/app/story.service.spec.ts
+++ rsystem-ui/src/app/story.service.spec.ts
@@ -10,7 +10,5 @@ describe('StoryService', () => {
     service = TestBed.inject(StoryService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+ 
 });
